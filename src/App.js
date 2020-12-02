9638839a8de4d439e968639ff0c3119ff562b42f
Oguzhan Alagoz <ogzogz88@gmail.com>
2020-12-03 01:48:55 +0300

    Add covid19 info with 4 InfoBoxes based on country

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react'
-import { FormControl, InputLabel, Select, MenuItem, } from '@material-ui/core'
+import { FormControl, InputLabel, Select, MenuItem, Card, CardContent } from '@material-ui/core'
 import axios from 'axios'
 import './App.css';
+import InfoBox from './components/InfoBox';
+import Map from './components/Map'
 
-
-{/* APPLICATION STRUCTURE
-    Header
-    Title + Select input dropdown field
-    InfoBoxes * 3 (new case, recovered, deaths)
-    Table 
-    Graph
-    Map 
-    */}
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('worldwide');
-  const handleChange = (e) => {
-    setCountry(e.target.value);
+  const [countryName, setCountryName] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+
+  const handleChange = async (e) => {
+    const countryName = e.target.value;
+    const URL = countryName === "worldwide" ? "https://disease.sh/v3/covid-19/all" :
+      `https://disease.sh/v3/covid-19/countries/${countryName}`;
+
+    await axios.get(URL)
+      .then(response => response.data)
+      .then(data => {
+        setCountryName(countryName);
+        setCountryInfo(data);
+      });
   }
 
   useEffect(() => {
@@ -25,42 +29,108 @@ function App() {
         .then(response => response.data)
         .then(data => {
           console.log(data);
+          let idNumber = 0;
           const countries = data.map(element => ({
             name: element.country,
-            value: element.countryInfo.iso2
+            value: element.countryInfo.iso2,
+            id: idNumber++
           }));
           setCountries(countries);
         });
     }
     getCountriesData();
+
+    const getWorldInfo = async () => {
+      await axios.get("https://disease.sh/v3/covid-19/all")
+        .then(response => response.data)
+        .then(data => {
+          setCountryInfo(data);
+        });
+    }
+    getWorldInfo();
   }, []);
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>Covid19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          <InputLabel id="covid-countries">Select</InputLabel>
-          <Select
-            variant="outlined"
-            labelId="covid-countries"
-            id="covid-countries-select"
-            value={country}
-            onChange={handleChange}
-            label="Country"
-          >
-            <MenuItem value="worldwide">
-              <em>Worldwide</em>
-            </MenuItem>
-            {countries.length > 0 &&
-              countries.map((country) => {
-                return (
-                  <MenuItem value={country.name}>{country.name}</MenuItem>
-                );
-              })
-            }
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1 className="app__title"><span className="app__covid">Covid19</span>info</h1>
+          <img className="app__flag" src={countryInfo?.countryInfo?.flag ?? "assets/worldFlag.png"} alt="" />
+          <FormControl className="app__dropdown">
+            <InputLabel id="covid-countries">Select</InputLabel>
+            <Select
+              variant="outlined"
+              labelId="covid-countries"
+              id="covid-countries-select"
+              value={countryName}
+              onChange={handleChange}
+              label="Country"
+            >
+              <MenuItem id={1000} value="worldwide">
+                <em>Worldwide</em>
+              </MenuItem>
+              {countries.length > 0 &&
+                countries.map((country) => {
+                  return (
+                    <MenuItem key={country.id} value={country.name}>{country.name}</MenuItem>
+                  );
+                })
+              }
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <div className="infobox__wrapper">
+            <InfoBox
+              img={'assets/caseImg.png'}
+              title={"Cases"}
+              caseTitle={"Daily Number"}
+              totalTitle={"Total Number"}
+              casesNumber={countryInfo.todayCases ? countryInfo.todayCases.toLocaleString() : ''}
+              totalNumber={countryInfo.cases ? countryInfo.cases.toLocaleString() : ''}
+
+            />
+          </div>
+          <div className="infobox__wrapper">
+            <InfoBox
+              img={'assets/recovered.png'}
+              title={"Recovered"}
+              caseTitle={"Daily Number"}
+              totalTitle={"Total Number"}
+              casesNumber={countryInfo.todayRecovered ? countryInfo.todayRecovered.toLocaleString() : ''}
+              totalNumber={countryInfo.recovered ? countryInfo.recovered.toLocaleString() : ''}
+            />
+          </div>
+          <div className="infobox__wrapper">
+            <InfoBox
+              img={'assets/current.png'}
+              title={"Current"}
+              caseTitle={"Active"}
+              totalTitle={"Critical"}
+              casesNumber={countryInfo.active ? countryInfo.active.toLocaleString() : ''}
+              totalNumber={countryInfo.critical ? countryInfo.critical.toLocaleString() : ''}
+            />
+          </div>
+          <div className="infobox__wrapper">
+            <InfoBox
+              img={'assets/death.png'}
+              title={"Deaths"}
+              caseTitle={"Daily Number"}
+              totalTitle={"Total Number"}
+              casesNumber={countryInfo.todayDeaths ? countryInfo.todayDeaths.toLocaleString() : ''}
+              totalNumber={countryInfo.deaths ? countryInfo.deaths.toLocaleString() : ''}
+            />
+          </div>
+        </div>
+
+        <Map />
       </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/components/InfoBox.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Card, CardHeader, CardMedia, CardContent, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const colors = {
+    colorCases: "#eb5569",
+    colorRecovered: "#46b29d",
+    colorCurrent: "#5c5b5b",
+    colorDeaths: "#000"
+};
+const useStyles = makeStyles(() => ({
+    root: {
+
+
+    },
+    media: {
+        height: 50,
+        width: 50,
+    },
+    header: {
+        paddingBottom: 0,
+    }
+}));
+function InfoBox({ title, caseTitle, casesNumber, totalTitle, totalNumber, img }) {
+    const classes = useStyles();
+    const date = new Date().toLocaleDateString();
+    return (
+        <Card className={classes.root} style={{ borderTop: `5px solid ${colors[`color${title}`]}` }}>
+            <CardHeader
+                className={classes.header}
+                avatar={
+                    <CardMedia
+                        component="img"
+                        className={`${classes.media} ${"infobox__media"}`}
+                        image={img}
+                        title={title}
+                    />
+                }
+                title={title}
+                subheader={date}
+            />
+            <CardContent>
+                <h5> {caseTitle}</h5>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {title === "Current" ? "" : "+"} {casesNumber ? casesNumber : '0'}
+                </Typography>
+                <h5>{totalTitle} </h5>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {totalNumber ? totalNumber : '0'}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
